5bbb3a017ae2b9e34f033a0ae6463d759fad1c57
J22David22 <jaime.david94@gmail.com>
2022-10-21 12:04:24 -0500

    Se agregó el disenio de la actividad de registro.

--- app/src/main/java/com/example/projectc4g5/LoginActivity.kt
+++ app/src/main/java/com/example/projectc4g5/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.projectc4g5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var labelRegistrar: TextView = findViewById(R.id.labelRegistrar)
+
+        labelRegistrar.setOnClickListener{
+            //Toast.makeText(this,"abrir el registro", Toast.LENGTH_SHORT).show()
+
+            val intento2=Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intento2)
+        }
     }
 }
